663cd403b03448c941fa08ed47d35687dfd68b65
Shane Colton <colton.shane@gmail.com>
2019-12-11 12:06:11 -0500

    Encoder bugfixes.

--- base_cmd.srcs/sources_1/ip/Encoder_1.0/hdl/Encoder_v1_0.v
+++ base_cmd.srcs/sources_1/ip/Encoder_1.0/hdl/Encoder_v1_0.v
@@ -5,6 +5,26 @@ Encoderv1_0.v
 Top module of encoder/quantizer. Receives output data from the wavelet stages,
 quantizes and encodes it, and writes the compressed stream to PS DDR4 using a 
 250MHz x 128b AXI master. Configuration and control is done via an AXI-Lite slave.
+
+Copyright (C) 2019 by Shane W. Colton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 ===================================================================================
 */
 
@@ -648,16 +668,18 @@ begin
             for(j = 0; j < 16; j = j + 1)
             begin
                 c_RAM_addr[j] <= c_RAM_addr_update_concat[32*j+:32];
-                c_RAM_addr_update_complete <= 1'b1;
             end
+            c_RAM_addr_update_complete <= 1'b1;
         end : axi_addr_update
         else 
+        begin
             // Running.
             if(axi_txn_done)
             begin
                 c_RAM_addr[c_state] <= c_RAM_addr[c_state] + 32'h200;
             end
             c_RAM_addr_update_complete <= 1'b0;
+        end
     end
 end
 for(i = 0; i < 16; i = i + 1)
--- base_cmd.srcs/sources_1/ip/Encoder_1.0/hdl/Encoder_v1_0_S00_AXI.v
+++ base_cmd.srcs/sources_1/ip/Encoder_1.0/hdl/Encoder_v1_0_S00_AXI.v
@@ -5,6 +5,26 @@ Encoder_v1_0_S00_AXI.v
 AXI slave controller for controlling and configuring the quantizer/encoder.
 The slave registers are mapped to PS memory, so the ARM cores have access to the 
 encoder block as a peripheral.
+
+Copyright (C) 2019 by Shane W. Colton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 =================================================================================
 */
 
@@ -239,7 +259,7 @@ begin
     if ( S_AXI_ARESETN == 1'b0 )
     begin : s_axi_areset_block
         integer i;
-        for(i = 0; i < 16; i = i + 1)
+        for(i = 0; i < 44; i = i + 1)
         begin
             slv_reg[i] <= 0;
         end
