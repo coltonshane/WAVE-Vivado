43448e5dfa1365f1c8469beba5075c211715008e
colton_shane <colton.shane@gmail.com>
2020-08-03 10:34:15 -0400

    Use multiboot register and sotware reset to start application.

--- base_cmd.vitis/WAVE_Boot/src/main.c
+++ base_cmd.vitis/WAVE_Boot/src/main.c
@@ -22,9 +22,33 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+// Include Headers -----------------------------------------------------------------------------------------------------
+
 #include "main.h"
 #include "gpio.h"
 #include "supervisor.h"
+
+// Private Pre-Processor Definitions -----------------------------------------------------------------------------------
+
+#define APPLICATION_FLASH_OFFSET 0x1800000
+
+// Private Type Definitions --------------------------------------------------------------------------------------------
+
+// Private Function Prototypes -----------------------------------------------------------------------------------------
+
+void resetToApplication(void);
+
+// Public Global Variables ---------------------------------------------------------------------------------------------
+
+// Private Global Variables --------------------------------------------------------------------------------------------
+
+// Registers
+u32 * const CSU_MULTI_BOOT = (u32 * const) 0xFFCA0010;
+u32 * const RESET_CTRL = (u32 * const) 0xFF5E0218;
+
+// Interrupt Handlers --------------------------------------------------------------------------------------------------
+
+// Public Function Definitions -----------------------------------------------------------------------------------------
 
 int main()
 {
@@ -50,7 +74,21 @@ int main()
 	}
 
 	print("WAVE Goodbye! Bootloader exit.\n\r");
+	resetToApplication();
 
     cleanup_platform();
     return 0;
+}
+
+// Private Function Definitions ----------------------------------------------------------------------------------------
+
+void resetToApplication(void)
+{
+	// Set multi-boot register to application offset. Offset unit is [32KiB].
+	*CSU_MULTI_BOOT = APPLICATION_FLASH_OFFSET >> 15;
+
+	// Trigger a soft reset.
+	*RESET_CTRL |= 0x10;
+
+	while(1);
 }
